cd63d8211930b2a4a70b67c19766386de7cae37e
Lea Ellingwood <leaellingwood@hotmail.com>
2018-10-16 10:18:25 -0400

    first commit

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ const publicFolderPath = path.join(__dirname, "public")
 app.use(express.json())
 app.use(express.static(publicFolderPath))
 
+const port = 3001
+
 const users = []
 
-// add POST request listener here
+app.post("/api/user/", (request, response) => {
 
-app.listen(3000);
+    if(users.find(newUser => newUser.username === request.body.username)){
+        response.status(409).send("error: user already exists")
+    } else {
+        request.body.userIDNumber = Math.floor(Math.random() * 333666999);
+        users.push(request.body);
+        response.status(201).send(request.body);
+    }
+})
+
+app.listen(port);
--- public/index.js
+++ public/index.js
@@ -1,2 +1,36 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+userCreateForm.addEventListener("submit", collateData) //the parentheses that would normally appear after collateDatat(since it's a function) are left out because including parentheses would run the function automatically.  here, you just want the information from the function here.
+
+function collateData(event){
+    event.preventDefault()
+    console.log("hello")
+    const email = document.getElementById("email").value
+    const username = document.getElementById("username").value
+    const firstname = document.getElementById("firstname").value
+    const lastname = document.getElementById("lastname").value
+    const fields = {email:email, username: username, firstname:firstname, lastname: lastname}
+    console.log(fields)
+    const stringifiedFields = JSON.stringify(fields)
+    
+    fetch("/api/user", {
+        method: "POST",
+        body: stringifiedFields,
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        }
+
+        //fetch needed to be inside the function.
+    })
+    .then(response => {
+        if(response.status === 201){
+            alert("success")
+        } else if (response.status === 409){
+            alert("fail")
+        }
+        return response.json() 
+    })
+    .then(response => {
+        console.log(response)
+    })
+};
